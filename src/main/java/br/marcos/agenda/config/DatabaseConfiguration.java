52a2e6be438c19333ceca27cbe70d3fa471f9c37
Marquinhos-c <marquinhos.cs08@gmail.com>
2026-06-14 09:52:12 -0400

    Configuracao do poll de coneccoes

---
+++ src/main/java/br/marcos/agenda/config/DatabaseConfiguration.java
@@ -0,0 +1,45 @@
+package br.marcos.agenda.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+
+    @Bean
+    public DataSource hikariDataSource() {
+
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setPoolName("agenda-db-pool");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+
+        return new HikariDataSource(config);
+
+    }
+}
